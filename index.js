50093d42acae7ea6cf026ff3cdaf792e8cf6592b
Dinesh Kumar <101325099+Dineshyagnik@users.noreply.github.com>
2024-12-12 16:17:22 +0530

    ejs  setup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 const { connectToMongoDB } = require('./connect');
 const urlRoute = require('./routes/url');
+const staticRoute = require('./routes/staticRouter');
 const URL = require('./models/url');
 
 const app = express();
@@ -10,9 +12,39 @@ connectToMongoDB('mongodb://localhost:27017/short-url')
     .then(() => console.log('MongoDb Connected'))
 
 
-app.use(express.json())
+app.set("view engine", "ejs");
+app.set("views", path.resolve('./views'))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+
+
+// app.get("/test",async(req,res)=>{
+//     const allUrls = await URL.find({});
+//     // return res.end(`
+//     //     <html>
+//     //     <head></head>
+//     //     <body>
+//     //         <ol>
+//     //             ${allUrls.map(url=>`<li>${url.shortId} - ${url.redirectURL} - ${url.visitHistory.length}</li>`).join('')}
+//     //         </ol>
+//     //     </body>
+//     //     </html>
+
+//     //     `);
+//     return res.render('home',{
+//         urls:allUrls,
+//     })
+// });
+
+
 app.use('/url', urlRoute);
-app.get('/:shortId', async (req, res) => {
+app.use('/', staticRoute)
+
+
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
@@ -20,8 +52,8 @@ app.get('/:shortId', async (req, res) => {
         },
         {
             $push: {
-                visitHistory:{
-                    timestamps:Date.now(),
+                visitHistory: {
+                    timestamps: Date.now(),
                 }
             },
         }
